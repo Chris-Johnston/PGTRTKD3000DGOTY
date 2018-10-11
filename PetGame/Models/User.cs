67bf6a7f5b0a986d6e23826c96a7f5494d50cbec
Chris Johnston <chris@thejohnstons.net>
2018-10-11 11:05:43 -0700

    Initial commit

---
+++ PetGame/Models/User.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PetGame.Models
+{
+    /// <summary>
+    ///     Represents information about a user.
+    /// </summary>
+    public class User
+    {
+        /// <summary>
+        ///     A user's unique identifier.
+        /// </summary>
+        public ulong UserId { get; set; }
+
+        //TODO: Define a Regex for checking validity of usernames.
+
+        /// <summary>
+        ///     A user's name.
+        ///     This must not be null or whitespace, and must be less than 32 characters in length.
+        ///     It should match a-zA-Z0-9!?.
+        /// </summary>
+        public string Username { get; set; }
+
+        //TODO: Define a regex for minimum (plaintext) password requirements.
+
+        /// <summary>
+        ///     Represents a user's hash value.
+        /// </summary>
+        /// <remarks>
+        ///     We will use a HMAC SHA512 password hash method.
+        ///     See https://docs.microsoft.com/en-us/dotnet/api/system.security.cryptography.hmacsha512
+        /// </remarks>
+        public byte[] PasswordHash { get; set; }
+
+        /// <summary>
+        ///     Represents the unique key that is used when checking this user's HMAC SHA512 password hash.
+        ///     This should be randomly-generated and unique for this user, and also combined with a constant 
+        ///     and known key stored elsewhere.
+        ///     This information should be kept very private.
+        /// </summary>
+        /// <remarks>
+        ///     This byte array will be 64 bytes in length.
+        /// </remarks>
+        public byte[] HMACKey { get; set; }
+    }
+}
---
+++ PetGame/Util/Cryptography.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using PetGame.Models;
+
+namespace PetGame.Util
+{
+    public class Cryptography
+    {
+        /// <summary>
+        ///     Verifies the given plain-text password with a 
+        ///     <see cref="User"/> to ensure that the hashes
+        ///     match.
+        /// </summary>
+        /// <returns>
+        ///     True if the passwords matched, false otherwise.
+        /// </returns>
+        /// <param name="plaintextPassword">
+        ///     The user's plaintext password, as they typed it into the form.
+        ///     (Should we hash this client-side?)
+        /// </param>
+        /// <param name="user">
+        ///     A reference to the <see cref="User"/> instance that this
+        ///     user is trying to authenticate as.
+        /// </param>
+        /// <exception cref="ArgumentNullException">
+        ///     Thrown if either the user or password parameters are null.
+        /// </exception>
+        public bool VerifyUserPassword(User user, string plaintextPassword)
+        {
+            if (user == null)
+                throw new ArgumentNullException(nameof(user), "The user parameter may not be null.");
+            if (string.IsNullOrWhiteSpace(plaintextPassword))
+                throw new ArgumentNullException(nameof(plaintextPassword), "The password may not be null or whitespace.");
+            // create a new HMAC SHA512 from the user's unique HMAC key
+            using (HMACSHA512 hmac = new HMACSHA512(user.HMACKey))
+            {
+                // create a reader for the pw string
+                using (var sreader = new MemoryStream(Encoding.ASCII.GetBytes(plaintextPassword)))
+                {
+                    var result = hmac.ComputeHash(sreader);
+
+                    return user.PasswordHash == result;
+                }
+            }
+            return false;
+        }
+    }
+}
