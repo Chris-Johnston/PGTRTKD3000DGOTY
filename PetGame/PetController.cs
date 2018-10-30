f2aa218530502e2b13d88bb1b2b3fedbd0627d55
Chris Johnston <chris@thejohnstons.net>
2018-10-29 18:06:20 -0700

    add some TODOs to PetController

--- PetGame/PetController.cs
+++ PetGame/PetController.cs
@@ -9,6 +9,8 @@ using PetGame.Models;
 
 namespace PetGame
 {
+    //TODO: This controller class is missing all Authorization logic, so that needs to be added
+
     [Route("api/[controller]")]
     public class PetController : Controller
     {
@@ -16,35 +18,85 @@ namespace PetGame
         [HttpGet]
         public IEnumerable<Pet> Get()
         {
+            // gets all of the pets
+            // not sure if we should actually support this endpoint
             return new List<Pet>()
             {
-                new Pet() { Birthday = DateTime.Now, Endurance = 50, IsDead = false, Name = "Bobby Tables", PetId = 123, Strength = 50, UserId = 1 }
+                new Pet()
+                {
+                    Birthday = DateTime.Now,
+                    Endurance = 50,
+                    IsDead = false,
+                    Name = "Bobby Tables",
+                    PetId = 123,
+                    Strength = 50,
+                    UserId = 1
+                },
+                new Pet()
+                {
+                    Birthday = DateTime.Today,
+                    Endurance = 10,
+                    IsDead = true,
+                    Name = "King Tables the Fourth",
+                    PetId = 111,
+                    Strength = 10,
+                    UserId = 2
+                }
             };
         }
 
         // GET api/<controller>/5
+        /// <summary>
+        ///     Gets a Pet by Id.
+        /// </summary>
+        /// <param name="id">The ID of the Pet to get from the database.</param>
+        /// <returns> A pet of the given ID, or null if unspecified. </returns>
         [HttpGet("{id}")]
         public Pet Get(ulong id)
         {
             return new Pet() { Birthday = DateTime.Now, Endurance = 50, IsDead = false, Name = "Bobby Tables", PetId = 123, Strength = 50, UserId = id };
         }
 
+        /// <summary>
+        ///     Inserts a new Pet into the database.
+        /// </summary>
+        /// <param name="value"> The pet to add to the database. </param>
         // POST api/<controller>
         [HttpPost]
         public void Post([FromBody]Pet value)
         {
+            if (value == null) throw new ArgumentNullException(nameof(value), "The supplied Pet cannot be null.");
+
+            //TODO Add the pet to the database.
         }
 
+        /// <summary>
+        ///     Updates a pet in the database.
+        /// </summary>
+        /// <param name="id">
+        ///     The ID of the pet to update.
+        /// </param>
+        /// <param name="value">
+        ///     The values of the pet to update.
+        /// </param>
         // PUT api/<controller>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
+        public void Put(ulong id, [FromBody]Pet value)
         {
+            if (value == null) throw new ArgumentNullException(nameof(value), "The supplied Pet cannot be null.");
+
+            // TODO: PetController PUT updates
         }
 
+        /// <summary>
+        ///     Deletes a pet from the database.
+        /// </summary>
+        /// <param name="id"></param>
         // DELETE api/<controller>/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public void Delete(ulong id)
         {
+            //TODO: Delete this pet from the database.
         }
     }
 }
