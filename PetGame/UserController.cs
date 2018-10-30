92b1e6b5fdb07c29d29b8d485af318cd0313233d
Chris Johnston <chris@thejohnstons.net>
2018-10-29 19:34:16 -0700

    remove unused UserController endpoints

--- PetGame/UserController.cs
+++ PetGame/UserController.cs
@@ -35,30 +35,5 @@ namespace PetGame
             //return Json(user);
             return Content($"Hello {user.Username}");
         }
-
-        // GET api/<controller>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/<controller>
-        [HttpPost]
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/<controller>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/<controller>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
     }
 }
