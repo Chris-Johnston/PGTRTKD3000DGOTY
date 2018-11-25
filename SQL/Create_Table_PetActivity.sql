2bc3199dc825b8ccf56303d030544828d2f71640
Saam Amiri <saam.amiri@gmail.com>
2018-11-25 15:48:47 -0800

    fixed changes

--- SQL/Create_Table_PetActivity.sql
+++ SQL/Create_Table_PetActivity.sql
@@ -1,21 +1,27 @@
--- Drop the table if it already exists
-IF OBJECT_ID('PetGame.PetActivity', 'U') IS NOT NULL
-DROP TABLE PetGame.PetActivity
 GO
--- Create the table in the specified schema
-CREATE TABLE PetGame.PetActivity
+USE PetGame
+
+/**
+PetActivity Table keeps track of all activites PET's do in the PetGame
+
+Some activites that are included are Training and Default
+along with these meta data is stored such as timestamp of activty
+**/
+CREATE TABLE PetActivity
 (
     -- primary key for for table
     ActivityId BIGINT IDENTITY(1,1) NOT NULL PRIMARY KEY, 
     
-    -- foreign key from PET.PetId
-    PetId BIGINT NOT NULL FOREIGN KEY REFERENCES PET(PetId),
+    -- foreign key from PET.PetId assume not null
+    PetId BIGINT FOREIGN KEY REFERENCES dbo.Pet(PetId)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
     
     -- timestamp of activty
-    Timestamp DateTime NOT NULL,   
+    Timestamp DateTime NOT NULL DEFAULT GETDATE(),   
     
     -- type of activty done by pet
-    Type CHAR(1) NOT NULL,
+    Type CHAR(1) NOT NULL DEFAULT 'd',
     -- checks if activity type is valid
     CHECK (Type in ('d', 't'))
 );
