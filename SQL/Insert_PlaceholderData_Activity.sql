fba61346936159b4f73e20be486cd61fa5fff461
Saam Amiri <Saam.amiri@gmail.com>
2018-11-25 17:31:43 -0800

    added data for activity (#9)
    * added data for activity
    
    * removed activity type
    
    * removed primary key data
    
    * Add the activity file to the sln
    
    * added a few GetDate()data entries

---
+++ SQL/Insert_PlaceholderData_Activity.sql
@@ -0,0 +1,14 @@
+USE [PetGame] 
+GO
+ -- inserts data into the Activty table
+ -- resembles data assocaited with pets
+INSERT INTO ACTIVITY (PetId, TimeStamp) VALUES (1, '03/07/2018 10:50:50 PM');
+INSERT INTO ACTIVITY (PetId, TimeStamp) VALUES (2, GetDate());
+INSERT INTO ACTIVITY (PetId, TimeStamp) VALUES (3, '03/09/2018 10:50:50 PM');
+INSERT INTO ACTIVITY (PetId, TimeStamp) VALUES (4, '03/10/2018 10:50:50 PM');
+INSERT INTO ACTIVITY (PetId, TimeStamp) VALUES (5, '03/11/2018 10:50:50 PM');
+INSERT INTO ACTIVITY (PetId, TimeStamp) VALUES (6, '03/12/2018 10:50:50 PM');
+INSERT INTO ACTIVITY (PetId, TimeStamp) VALUES (7, GetDate());
+INSERT INTO ACTIVITY (PetId, TimeStamp) VALUES (8, GetDate());
+INSERT INTO ACTIVITY (PetId, TimeStamp) VALUES (9, '03/15/2018 10:50:50 PM');
+INSERT INTO ACTIVITY (PetId, TimeStamp) VALUES (10, '03/16/2018 10:50:50 PM');
