2e22a7abdf27b64825fa1e71c287a853d2d7cded
Saam Amiri <saam.amiri@gmail.com>
2018-11-25 17:01:57 -0800

    removed activity type

--- SQL/Insert_PlaceholderData_Activity.sql
+++ SQL/Insert_PlaceholderData_Activity.sql
@@ -1,10 +1,14 @@
-INSERT INTO ACTIVITY (ActivityId, PetId, TimeStamp, Type) VALUES ('1', '1', '03/07/2018 10:50:50 PM', 'd');
-INSERT INTO ACTIVITY (ActivityId, PetId, TimeStamp, Type) VALUES ('2', '2', '03/08/2018 10:50:50 PM', 'd');
-INSERT INTO ACTIVITY (ActivityId, PetId, TimeStamp, Type) VALUES ('3', '3', '03/09/2018 10:50:50 PM', 'd');
-INSERT INTO ACTIVITY (ActivityId, PetId, TimeStamp, Type) VALUES ('4', '4', '03/10/2018 10:50:50 PM', 'd');
-INSERT INTO ACTIVITY (ActivityId, PetId, TimeStamp, Type) VALUES ('5', '5', '03/11/2018 10:50:50 PM', 'd');
-INSERT INTO ACTIVITY (ActivityId, PetId, TimeStamp, Type) VALUES ('6', '6', '03/12/2018 10:50:50 PM', 't');
-INSERT INTO ACTIVITY (ActivityId, PetId, TimeStamp, Type) VALUES ('7', '7', '03/13/2018 10:50:50 PM', 't');
-INSERT INTO ACTIVITY (ActivityId, PetId, TimeStamp, Type) VALUES ('8', '8', '03/14/2018 10:50:50 PM', 't');
-INSERT INTO ACTIVITY (ActivityId, PetId, TimeStamp, Type) VALUES ('9', '9', '03/15/2018 10:50:50 PM', 't');
-INSERT INTO ACTIVITY (ActivityId, PetId, TimeStamp, Type) VALUES ('10', '10', '03/16/2018 10:50:50 PM', 't');
+USE [PetGame] 
+GO
+ -- inserts data into the Activty table
+ -- resembles data assocaited with pets
+INSERT INTO ACTIVITY (ActivityId, PetId, TimeStamp) VALUES (1, 1, '03/07/2018 10:50:50 PM');
+INSERT INTO ACTIVITY (ActivityId, PetId, TimeStamp) VALUES (2, 2, '03/08/2018 10:50:50 PM');
+INSERT INTO ACTIVITY (ActivityId, PetId, TimeStamp) VALUES (3, 3, '03/09/2018 10:50:50 PM');
+INSERT INTO ACTIVITY (ActivityId, PetId, TimeStamp) VALUES (4, 4, '03/10/2018 10:50:50 PM');
+INSERT INTO ACTIVITY (ActivityId, PetId, TimeStamp) VALUES (5, 5, '03/11/2018 10:50:50 PM');
+INSERT INTO ACTIVITY (ActivityId, PetId, TimeStamp) VALUES (6, 6, '03/12/2018 10:50:50 PM');
+INSERT INTO ACTIVITY (ActivityId, PetId, TimeStamp) VALUES (7, 7, '03/13/2018 10:50:50 PM');
+INSERT INTO ACTIVITY (ActivityId, PetId, TimeStamp) VALUES (8, 8, '03/14/2018 10:50:50 PM');
+INSERT INTO ACTIVITY (ActivityId, PetId, TimeStamp) VALUES (9, 9, '03/15/2018 10:50:50 PM');
+INSERT INTO ACTIVITY (ActivityId, PetId, TimeStamp) VALUES (10, 10, '03/16/2018 10:50:50 PM');
