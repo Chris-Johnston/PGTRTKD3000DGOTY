2307bd046d932fdfda44e298c5fb01740a958119
Anish Prasad <anishnp01@gmail.com>
2018-11-28 14:46:41 -0800

    Added query

--- PetGame/Controllers/LeaderboardController.cs
+++ PetGame/Controllers/LeaderboardController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using PetGame.Core;
 using PetGame.Models;
 
 namespace PetGame
@@ -10,19 +11,36 @@ namespace PetGame
     [Route("api/[controller]")]
     public class LeaderboardController : Controller
     {
+        private readonly SqlManager sqlManager;
+
         [HttpGet]
-        public IEnumerable<Race> Get()
+        public IActionResult Get()
         {
             //create list to hold the races
-            List<Race> TopTenRaces = new List<Race>();
+            //List<Race> TopTenRaces = new List<Race>();
+            List<string> TopTenRaces = new List<string>();
 
             //Query db for races
-            
-            //sort by score in descending order
-            TopTenRaces.Sort((x, y) => y.Score.CompareTo(x.Score));
+            using (var conn = sqlManager.EstablishDataConnection)
+            {
+                var cmd = conn.CreateCommand();
 
+                cmd.CommandText = @"SELECT TOP (10) Pet.[Name] AS 'Pet Name', Race.Score, [User].Username AS 'Owner Name' FROM Pet, Race, [User] WHERE Race.PetId = Pet.PetId AND [User].UserId = Pet.UserId ORDER BY Score DESC;";
+
+                using (var reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        for (int i = 0; i < 10; i++)
+                        {
+                            TopTenRaces.Add(reader.GetString(i));
+                        }
+                        
+                    }
+                }
+            }
             //return races
-            return TopTenRaces;
+            return Json(TopTenRaces);
         }
     }
 }
