984329e0102bc2fb7b44e2c74f42e70662fde4ab
Chris Johnston <chjohnston@protonmail.com>
2018-11-27 17:28:50 -0800

    cleanup codefactor warning

--- PetGame/GlobalSuppressions.cs
+++ PetGame/GlobalSuppressions.cs
@@ -1,9 +1,7 @@
-﻿
-// This file is used by Code Analysis to maintain SuppressMessage 
+﻿// This file is used by Code Analysis to maintain SuppressMessage 
 // attributes that are applied to this project.
 // Project-level suppressions either have no target or are given 
 // a specific target and scoped to a namespace, type, member, etc.
 
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "CA2208:Instantiate argument exceptions correctly", Justification = "<Pending>", Scope = "member", Target = "~M:PetGame.LoginService.GetUserToken(Microsoft.AspNetCore.Mvc.ControllerBase)~PetGame.Models.UserToken")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "CA2208:Instantiate argument exceptions correctly", Justification = "<Pending>", Scope = "member", Target = "~M:PetGame.LoginService.RegisterNewUser(Microsoft.AspNetCore.Mvc.ControllerBase)~PetGame.Models.UserToken")]
-
