3cca7175642770185002c90a136dd60c886de59d
Anish Prasad <anishnp01@gmail.com>
2018-12-01 15:28:34 -0800

    Added check for incongrous List lengths, and condensed code from 3 for loops to 1

--- PetGame/Controllers/LeaderboardController.cs
+++ PetGame/Controllers/LeaderboardController.cs
@@ -35,7 +35,9 @@ namespace PetGame
                 var cmd = conn.CreateCommand();
 
                 //to be optimized later, if time allows
-                cmd.CommandText = @"SELECT TOP (10) Pet.[Name] AS 'Pet Name', Race.Score, [User].Username AS 'Owner Name' FROM Pet, Race, [User] WHERE Race.PetId = Pet.PetId AND [User].UserId = Pet.UserId ORDER BY Score DESC;";
+                cmd.CommandText = @"SELECT TOP (10) Pet.[Name] AS 'Pet Name', Race.Score, [User].Username AS 'Owner Name' 
+                                    FROM Pet, Race, [User] 
+                                    WHERE Race.PetId = Pet.PetId AND [User].UserId = Pet.UserId ORDER BY Score DESC;";
 
                 //store the pet names, scores, and owner names in the lists
                 using (var reader = cmd.ExecuteReader())
@@ -52,7 +54,15 @@ namespace PetGame
                 //if any of the lists are empty, there is no usable data
                 //return an empty table
                 if (TopTenScores.Count <= 0 || TopTenPetNames.Count <= 0 ||
-                    TopTenOwnerNames.Count <= 0)
+                        TopTenOwnerNames.Count <= 0)
+                {
+                    //returns empty table full of nulls
+                    return Json(table);
+                }
+                //if the lists do not have equal lengths, there is no usable data
+                //return an empty table
+                else if ((TopTenScores.Count != TopTenPetNames.Count) ||
+                            (TopTenPetNames.Count != TopTenOwnerNames.Count))
                 {
                     //returns empty table full of nulls
                     return Json(table);
@@ -60,19 +70,14 @@ namespace PetGame
                 //if there is data, add it to the table
                 else
                 {
-                    //add the pet names to index 0
+                    //add the data from the lists to the mulidimensional array/table
                     for (int i = 0; i < TopTenPetNames.Count; i++)
                     {
+                        //add the pet names to index 0
                         table[0, i] = TopTenPetNames[i];
-                    }
-                    //add the scores to index 1
-                    for (int i = 0; i < TopTenScores.Count; i++)
-                    {
+                        //add the scores to index 1
                         table[1, i] = TopTenScores[i].ToString();
-                    }
-                    //add the owner names to index 2
-                    for (int i = 0; i < TopTenOwnerNames.Count; i++)
-                    {
+                        //add the owner names to index 2
                         table[2, i] = TopTenOwnerNames[i];
                     }
                 }
