3a86b443159fca947e9969471a8e481d14f2e32a
Anish Prasad <anishnp01@gmail.com>
2018-12-01 22:35:25 -0800

    Created new type: LeaderboardEntry. Modified LeaderboardController to use the new type. Removed hardcoded numbers.

--- PetGame/Controllers/LeaderboardController.cs
+++ PetGame/Controllers/LeaderboardController.cs
@@ -5,29 +5,37 @@ using PetGame.Core;
 
 namespace PetGame
 {
+    public class LeaderboardEntry
+    {
+        public string PetName { get; set; }
+        public int score { get; set; }
+        public string OwnerName { get; set; }
+
+        public LeaderboardEntry(string nPetName, int nScore, string nOwnerName)
+        {
+            PetName = nPetName;
+            score = nScore;
+            OwnerName = nOwnerName;
+        }
+    }
+
     [Route("api/[controller]")]
     public class LeaderboardController : Controller
     {
         //initialize connection to SQL Server
         private readonly SqlManager sqlManager;
 
-        public LeaderboardController(SqlManager sqlManager) : base()
+        public LeaderboardController(SqlManager sqlManager)
         {
             this.sqlManager = sqlManager;
         }
 
         [HttpGet]
-        public IActionResult Get()
+        public IActionResult Get(int NumResults = 10)
         {
-            //create lists to hold the information
+            //create list to hold the information
             //names of pets on leaderboard
-            List<string> TopTenPetNames = new List<string>();
-            //scores of pets on leaderboard
-            List<Int32> TopTenScores = new List<Int32>();
-            //owner names of pets on leaderboard
-            List<string> TopTenOwnerNames = new List<string>();
-            //data structure to be returned as json
-            string[,] table = new string[3,10];
+            List<LeaderboardEntry> ScoreList = new List<LeaderboardEntry>();
             //Query db for races
             using (var conn = sqlManager.EstablishDataConnection)
             {
@@ -35,55 +43,25 @@ namespace PetGame
                 var cmd = conn.CreateCommand();
 
                 //to be optimized later, if time allows
-                cmd.CommandText = @"SELECT TOP (10) Pet.[Name] AS 'Pet Name', Race.Score, [User].Username AS 'Owner Name' 
-                                    FROM Pet, Race, [User] 
-                                    WHERE Race.PetId = Pet.PetId AND [User].UserId = Pet.UserId ORDER BY Score DESC;";
+                cmd.CommandText = @"SELECT TOP (" + NumResults + ") Pet.[Name] AS 'PetName', Race.Score, [User].Username AS 'OwnerName' " +
+                    "FROM Pet, Race, [User] WHERE Race.PetId = Pet.PetId AND [User].UserId = Pet.UserId ORDER BY Score DESC;";
 
                 //store the pet names, scores, and owner names in the lists
                 using (var reader = cmd.ExecuteReader())
                 {
                     while (reader.Read())
                     {
-                        //add each pet name/score/owner name to a new index in the list
-                        TopTenPetNames.Add(reader.GetString(0));
-                        TopTenScores.Add(reader.GetInt32(1));
-                        TopTenOwnerNames.Add(reader.GetString(2));
-                    }
-                }
+                        string PetName = reader.GetString(0);
+                        int score = reader.GetInt32(1);
+                        string OwnerName = reader.GetString(2);
 
-                //if any of the lists are empty, there is no usable data
-                //return an empty table
-                if (TopTenScores.Count <= 0 || TopTenPetNames.Count <= 0 ||
-                        TopTenOwnerNames.Count <= 0)
-                {
-                    //returns empty table full of nulls
-                    return Json(table);
-                }
-                //if the lists do not have equal lengths, there is no usable data
-                //return an empty table
-                else if ((TopTenScores.Count != TopTenPetNames.Count) ||
-                            (TopTenPetNames.Count != TopTenOwnerNames.Count))
-                {
-                    //returns empty table full of nulls
-                    return Json(table);
-                }
-                //if there is data, add it to the table
-                else
-                {
-                    //add the data from the lists to the mulidimensional array/table
-                    for (int i = 0; i < TopTenPetNames.Count; i++)
-                    {
-                        //add the pet names to index 0
-                        table[0, i] = TopTenPetNames[i];
-                        //add the scores to index 1
-                        table[1, i] = TopTenScores[i].ToString();
-                        //add the owner names to index 2
-                        table[2, i] = TopTenOwnerNames[i];
+                        //add each pet name/score/owner name to a new index in the list
+                        ScoreList.Add(new LeaderboardEntry(PetName, score, OwnerName));
                     }
                 }
             }
             //return races
-            return Json(table);
+            return Json(ScoreList);
         }
     }
 }
