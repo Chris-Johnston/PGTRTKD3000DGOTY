42b71c531fe8062aabe0ad91f9575f2535a95d8e
Chris Johnston <chjohnston@protonmail.com>
2018-12-02 00:51:50 -0800

    Add scaffolding of what the index page might look like.

--- PetGame/Pages/Index.cshtml.cs
+++ PetGame/Pages/Index.cshtml.cs
@@ -4,13 +4,27 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using PetGame.Core;
+using PetGame.Models;
 
 namespace PetGame.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly SqlManager sql;
+        private readonly LoginService login;
+
+        public User CurrentUser { get; private set; } = null;
+
+        public IndexModel(SqlManager sql)
+        {
+            this.sql = sql;
+            login = new LoginService(this.sql);
+        }
+
         public void OnGet()
         {
+            CurrentUser = login.GetUserFromContext(HttpContext.User);
         }
     }
 }
