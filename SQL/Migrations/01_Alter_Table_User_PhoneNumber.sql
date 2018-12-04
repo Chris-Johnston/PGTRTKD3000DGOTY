931e4a7236a082c02a3a2d7ee98820d446285ede
Chris Johnston <chris@thejohnstons.net>
2018-12-03 16:21:22 -0800

    Fix #48 Add Phone Number column to User table
    Adds the alteration script for adding the PhoneNumber column to the User table.
    This is in the required format to be used by Twilio.

---
+++ SQL/Migrations/01_Alter_Table_User_PhoneNumber.sql
@@ -0,0 +1,21 @@
+﻿/*
+Migration #01, run these in order
+
+Alters the User table to add the PhoneNumber column.
+This defaults to a NULL value.
+Numbers are stored as strings, in the format:
++1XXXYYYZZZZ
+where XXX = area code
+YYYZZZZ = phone number.
+
+We are assuming that all phone numbers are located in the US.
+This is the format that is accepted by the Twilio API, which is what is planned for SMS.
+*/
+
+ALTER TABLE [User] ADD
+	PhoneNumber char(12) NULL DEFAULT(NULL)
+	CONSTRAINT PhoneNumber_IsNumeric
+	CHECK (PhoneNumber IS NULL OR PhoneNumber LIKE '+1[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]');
+
+-- sample usage
+-- UPDATE [User] Set PhoneNumber = '+14255555555' WHERE Username LIKE 'chris%';
