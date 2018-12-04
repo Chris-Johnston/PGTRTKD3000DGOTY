42a9d3f29691d758d7c2fd1eed09b9d6281d93dc
Chris Johnston <chjohnston@protonmail.com>
2018-12-03 20:40:35 -0800

    specify AfterBuild target for gulpfile

--- PetGame/Gulpfile.js
+++ PetGame/Gulpfile.js
@@ -1,4 +1,4 @@
-﻿/// <binding BeforeBuild='default' Clean='clean' />
+/// <binding BeforeBuild='default' AfterBuild='default' Clean='clean' />
 /*
 This file is the main entry point for defining Gulp tasks and using Gulp plugins.
 Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
