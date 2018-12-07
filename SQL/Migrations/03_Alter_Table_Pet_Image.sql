1ef0c5e80ff4cbb312506cd91f60553998ccea40
Chris Johnston <chris@thejohnstons.net>
2018-12-07 14:45:42 -0800

    Add script for altering the Pet table with the new petimageid column, and update the api to use this value

---
+++ SQL/Migrations/03_Alter_Table_Pet_Image.sql
@@ -0,0 +1,14 @@
+/**
+	Alters the Pet table to include the image number of the pet.
+	Because the image number is a hard-coded path that will never change, this information does not have to be stored in the database, 
+	and instead can be a hard-coded dict in the API code.
+*/
+
+ALTER TABLE Pet ADD
+	-- by default, all existing pets will have the default image
+	PetImageId INT DEFAULT(0)
+	CONSTRAINT PetImageId_IsNotNegative
+	CHECK (PetImageId >= 0);
+
+-- Sampe usage
+-- UPDATE Pet SET PetImageId = 123 WHERE PetId = 1;
