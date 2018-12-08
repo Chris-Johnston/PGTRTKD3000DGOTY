4b899de69a91651039022db8ecf49a5bdfbe9729
Anish Prasad <anishnp01@gmail.com>
2018-12-08 15:49:58 -0800

    Added services needed to Race codebehind

--- PetGame/Pages/Race.cshtml.cs
+++ PetGame/Pages/Race.cshtml.cs
@@ -4,13 +4,39 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using PetGame.Core;
+using PetGame.Models;
+using PetGame.Services;
 
 namespace PetGame.Pages
 {
     public class RaceModel : PageModel
     {
+        //pet strength, pet endurance,
+        //create race entry
+        //activity, race and pet tables
+
+        public Pet CurrentPet { get; private set; } = null;
+        public User CurrentUser { get; private set; } = null;
+
+        private readonly SqlManager sqlManager;
+        private readonly PetService petService;
+        private readonly ActivityService activityService;
+        private readonly RaceService raceService;
+        private readonly LoginService loginService;
+
+        public RaceModel(SqlManager sql)
+        {
+            sqlManager = sql;
+            petService = new PetService(sql);
+            activityService = new ActivityService(sql);
+            raceService = new RaceService(sql);
+            loginService = new LoginService(sql);
+        }
+
         public void OnGet()
         {
+            CurrentUser = loginService.GetUserFromContext(HttpContext.User);
         }
     }
 }
