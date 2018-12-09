0d1361c4193fac1316ab269c521268f9f890d2a5
Anish Prasad <anishnp01@gmail.com>
2018-12-08 19:01:33 -0800

    Removed non-working code

--- PetGame/Pages/Race.cshtml.cs
+++ PetGame/Pages/Race.cshtml.cs
@@ -18,20 +18,15 @@ namespace PetGame.Pages
 
         public Pet CurrentPet { get; private set; } = null;
         public User CurrentUser { get; private set; } = null;
-        public int CurrentScore { get; private set; } = 0;
 
         private readonly SqlManager sqlManager;
         private readonly PetService petService;
-        private readonly ActivityService activityService;
-        private readonly RaceService raceService;
         private readonly LoginService loginService;
 
         public RaceModel(SqlManager sql)
         {
             sqlManager = sql;
             petService = new PetService(sql);
-            activityService = new ActivityService(sql);
-            raceService = new RaceService(sql);
             loginService = new LoginService(sql);
         }
 
@@ -41,37 +36,6 @@ namespace PetGame.Pages
             //null checks performed on previous page
             CurrentUser = loginService.GetUserFromContext(HttpContext.User);
             CurrentPet = petService.GetPetById(id);
-        }
-
-        public bool InsertActivity()
-        {
-            if (CurrentScore == 0)
-            {
-                return false;
-            }
-            else
-            {
-                activityService.MakeActivityForPet(CurrentPet.PetId, ActivityType.Race);
-                return true;
-            }
-        }
-
-        public bool InsertRace()
-        {
-            if (CurrentScore == 0)
-            {
-                return false;
-            }
-            else
-            {
-                raceService.InsertRace(new Race() { Score = CurrentScore, Timestamp = DateTime.Now, PetId = CurrentPet.PetId });
-                return true;
-            }
-        }
-
-        public bool UpdatePet()
-        {
-            return false;
         }
     }
 }
