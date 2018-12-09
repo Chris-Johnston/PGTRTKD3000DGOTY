83ff904bd3dcd674a108cd85b4adc817b906fb66
Chris Johnston <chjohnston@protonmail.com>
2018-12-08 18:00:00 -0800

    Add jquery to the npm packages

---
+++ PetGame/scripts/petStatus.ts
@@ -0,0 +1,9 @@
+﻿// requires jQuery
+import $ from "jquery";
+
+// set up refs to buttons and spans in the page
+
+var petHunger = $("#pet-hunger")[0];
+var petHappy = $("#pet-happiness")[0];
+var petStrength = $("#pet-strength")[0];
+var petEndurance = $("#pet-strength")[0];
