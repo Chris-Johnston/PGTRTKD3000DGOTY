d60e634ceddf7ab33d5ca38bc8ae4b278eb8985c
Chris Johnston <chjohnston@protonmail.com>
2018-12-09 16:57:29 -0800

    massive commit of changes made to the race pagez

--- PetGame/scripts/race.ts
+++ PetGame/scripts/race.ts
@@ -4,129 +4,279 @@ declare const petimageid: number;
 declare const endurance: number;
 declare const strength: number;
 
-var button = document.getElementById("race-button");
+// rate that race button can be pressed 0-1000ms
+const minCooldown = 0;
+const maxCooldown = 3000;
 
-button.addEventListener('click', function () {
-    moveRight();
-    startCount();
-    cooldown();
+// range of possible values for endurance
+const minEndurance = 0;
+const maxEndurance = 100;
+const minStrength = 0;
+const maxStrength = 100;
+
+// determine the cooldown length for the endurance value, constrained to be between 
+// the worst and the best rate
+const enduranceRate = constrain(map(endurance, 0, 100, maxCooldown, minCooldown), maxCooldown, minCooldown);;
+
+// percentages of the total distance to move with each keypress
+const minDistance = 0.05;
+const maxDistance = 0.3;
+
+// how far the pet will run all the time
+const minNormalRunSpeed = 0.01;
+const maxNormalRunSpeed = 0.1;
+
+// how far across the total distance to move
+const moveDistance = constrain(map(strength, minStrength, maxStrength, minDistance, maxDistance), minDistance, maxDistance);
+// how far the pet will run in a second
+const runSpeed = constrain(map(strength, minStrength, maxStrength, minNormalRunSpeed, maxNormalRunSpeed), minNormalRunSpeed, maxNormalRunSpeed);
+
+var raceButton = document.getElementById("race-button");
+var startButton = document.getElementById("start-race") as HTMLButtonElement;
+
+var beforerace = document.getElementById("before-race") as HTMLDivElement;
+var duringrace = document.getElementById("during-race") as HTMLDivElement; 
+var afterRace = document.getElementById("after-race") as HTMLDivElement;
+
+var finalScore = document.getElementById("final-score") as HTMLSpanElement;
+
+var cooldownActive = false;
+
+var gameActive = false;
+
+raceButton.addEventListener('click', function () {
+    press();
 });
 
+function press() {
+    if (gameActive) {
+        if (!cooldownActive) {
+            moveRight();
+            startCount();
+            cooldown();
+        }
+        // already in cooldown
+        else {
+            errorShake();
+        }
+    }
+}
+
+var shake = true;
+function errorShake() {
+    if (shake) {
+        shake = false;
+        raceButton.classList.add("shaker");
+        setTimeout(function () {
+            raceButton.classList.remove("shaker");
+            shake = true;
+        }, 300);
+    }
+}
+
 var myGamePiece: any = null;
+// hey if you are looking to hack the game, you should change this variable :^)
+var distance = 0;
 
 function startGame() {
-    myGamePiece = component(20, 20, `/api/image/${petimageid}`, 10, 120);
+    myGamePiece = component(250, 250, `/api/image/${petimageid}`, 10, 120);
     myGameArea.start();
+    distance = 0;
+    gameActive = true;
 }
 
 var myGameArea = {
     canvas: document.getElementById("race-canvas"),
     start: function () {
         this.context = this.canvas.getContext("2d");
-        this.interval = setInterval(updateGameArea, 60);
+        this.interval = setInterval(updateGameArea, updateRate);
     },
     clear: function () {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     }
 }
 
-function component(width: number, height: number, color: any, x: any, y: any) {
+function getTotalDistance(): number {
+    // the total distance, minus the size of the image, and some padding
+    return (myGameArea.canvas as HTMLCanvasElement).width - 150 - 50;
+}
+
+function component(width: number, height: number, imageSrc: string, x: any, y: any) {
     this.width = width;
     this.height = height;
     this.speedX = 0;
     this.speedY = 0;
     this.x = x;
     this.y = y;
+    this.image = new Image();
+    this.image.src = imageSrc;
     var ctx = null;
-    this.update = function () {
+    this.update = function ()
+    {
+        updateRunning();
+        this.x = distance * getTotalDistance();
+        // draw the image on the canvas
         ctx = (myGameArea.canvas as HTMLCanvasElement).getContext("2d");
-        ctx.fillStyle = color;
-        ctx.fillRect(this.x, this.y, this.width, this.height);
+        ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
 
-        // checks if gamePiece crosses finish line
-        if (this.x >= (myGameArea.canvas as HTMLCanvasElement).width) {
-            ctx.fillStyle = "#1828D3";
-            ctx.fillRect(this.x, this.y, this.width, this.height);
-            clearInterval(60);
-            stopCount();
-            var score = Math.floor(1000 / seconds);
-
-            // assign score value from seconds
-            if (seconds <= 0) {
-                score = 1000;
-            }
-            else {
-                score = Math.floor(1000 / seconds);
-            }
-
-            //document.write("You finished the race in " + seconds + " seconds! <br> Score: " + score);
-            alert("You finished the race in " + seconds + " seconds! \n Score: " + score);
-        }
+        // draw the current time
+        ctx.font = "30px Arial";
+        ctx.fillText(`Time: ${seconds}`, 10, 50);
     }
-    this.newPos = function () {
-        this.x = this.speedX;
-    }
+    return this;
 }
 
 function updateGameArea() {
     myGameArea.clear();
-    myGamePiece.newPos();
     myGamePiece.update();
+
+    // check if distance >1
+    if (distance > 1) {
+        stopCount();
+        // placeholer
+        win();
+    }
 }
 
+function win()
+{
+    gameActive = false;
+
+    // determine the score
+    var score = Math.floor(map(seconds, 0, 60, 100000000, 10));
+    if (score < 0)
+        score = 0;
+    // set the final score
+    finalScore.innerText = `${score}`;
+
+    // post the score
+    // this is very insecure, but I don't really care atm
+    const to = `/api/pet/${petid}/race/${score}`;
+    var request = new XMLHttpRequest();
+    request.open('POST', to);
+    request.addEventListener('load', function (ev: ProgressEvent) {
+        if (this.status >= 300) {
+            console.log(this);
+            console.log(ev);
+            alert('got a non 200 status, uh oh.');
+        }
+            
+    })
+    request.send();
+    // does this need a callback?
+}
 
 
 var seconds = 0;
 var timer : number;
-var timer_is_on = 0;
+var timer_is_on : boolean = false;
 
 function timedCount() {
+    // would have preferred to use a DateTime here instead
     seconds++;
-    //document.getElementById("txt").value = seconds;
     timer = setTimeout(timedCount, 1000);
 }
 
 function startCount() {
     if (!timer_is_on) {
-        timer_is_on = 1;
+        timer_is_on = true;
         timedCount();
     }
 }
 
 function stopCount() {
     clearTimeout(timer);
-    timer_is_on = 0;
+    timer_is_on = false;
+}
+
+
+// constrains x to the range [min-max], inclusive
+function constrain(x: number, min: number, max: number) : number {
+    if (x < min) return min;
+    if (x > max) return max;
+    return x;
+}
+
+// equivalent to the arduino method
+// https://www.arduino.cc/reference/en/language/functions/math/map/
+function map(x: number, in_min: number, in_max: number, out_min: number, out_max: number) : number {
+    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+// sets the state of the cooldown, and the buttons affected by it
+function setCooldown(active: boolean) {
+    cooldownActive = active;
+    (raceButton as HTMLButtonElement).disabled = active;
 }
 
 // cooldown for button after each press, based off of pet endurance
 function cooldown() {
-    var btn = document.getElementById("race");
-
-    // rate that race button can be pressed 0-1000ms
-    var bestRate = 0;
-    var worstRate = 1000;
-    var enduranceRate = worstRate;
-
-    // assumes positive values between 0-100
-    if (endurance >= 100) {
-        enduranceRate = bestRate;
-    } else if (endurance <= 0) {
-        enduranceRate = worstRate;
-    } else {
-        // ranges values from 1000-0ms of button press wait time
-        enduranceRate = 1000 - (endurance * 10);
-    }
-
+    var btn = raceButton;
     // disable button for enduranceRate length
-    (btn as HTMLButtonElement).disabled = true;
+    setCooldown(true);
     setTimeout(function () {
-        (btn as HTMLButtonElement).disabled = false;
+        setCooldown(false);
     }, enduranceRate);
 }
-function moveRight() {
-    var distance = (myGameArea.canvas as HTMLCanvasElement).width * (strength / 500);
-    myGamePiece.speedX += (distance);
 
+const updateRate = 60;
+
+function updateRunning() {
+    if (gameActive) {
+        distance += runSpeed / updateRate;
+    }
 }
 
-startGame();
+function moveRight() {
+    if (gameActive) {
+        distance += moveDistance;
+    }
+}
+// prevent the user from being able to hold the spacebar down
+var spaceDown = false;
+document.addEventListener('keydown', function (ev: KeyboardEvent) {
+    if (ev.key == " ") {
+        if (spaceDown) return;
+        spaceDown = true;
+        press();
+    }
+}, false);
+
+document.addEventListener('keyup', function (ev: KeyboardEvent) {
+    if (ev.key == " ") {
+        spaceDown = false;
+    }
+}, false);
+
+// button for starting the game, that hides itself when clicked
+startButton.addEventListener('click', function () {
+    countdown();
+    startButton.disabled = true;
+}, false);
+
+var countdowntext = document.getElementById("countdowntext") as HTMLHeadingElement;
+
+var startingCountdown = false;
+function countdown() {
+    if (startingCountdown) return;
+    startingCountdown = true;
+    countdowntext.style.display = "block";
+    countdowntext.innerHTML = `Race begins in 3...`;
+    setTimeout(function () {
+        countdowntext.innerHTML = `Race begins in 2...`;
+    }, 1000);
+    setTimeout(function () {
+        countdowntext.innerHTML = `Race begins in 1...`;
+    }, 2000);
+    setTimeout(function () {
+        actuallyStart();
+    }, 3000);
+}
+
+
+function actuallyStart() {
+    startGame();
+    duringrace.style.display = "block";
+    beforerace.style.display = "none";
+}
+
