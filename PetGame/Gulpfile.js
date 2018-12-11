55209e4ed1a552df05e1ae3ce6a321f6bfa66bff
Chris Johnston <chjohnston@protonmail.com>
2018-12-10 20:21:35 -0800

    update gulpfile to debug files being built

--- PetGame/Gulpfile.js
+++ PetGame/Gulpfile.js
@@ -7,6 +7,7 @@ Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
 var gulp = require('gulp');
 var del = require('del');
 var less = require('gulp-less');
+var debug = require('gulp-debug');
 
 var paths = {
     scripts: ['scripts/**/*.js', 'scripts/**/*.ts', 'scripts/**/*.map'],
@@ -22,13 +23,16 @@ gulp.task('cleanless', function () {
 gulp.task('clean', gulp.parallel(['cleanscripts', 'cleanless']));
 
 gulp.task('scripts', function () {
-    return gulp.src(paths.scripts).pipe(gulp.dest('wwwroot/scripts'));
+    return gulp.src(paths.scripts)
+        .pipe(gulp.dest('wwwroot/scripts'))
+        .pipe(debug({ title: 'scripts output:' }));
 });
 
 gulp.task('less', function () {
     return gulp.src('style/main.less')
         .pipe(less())
-        .pipe(gulp.dest('wwwroot/css'));
+        .pipe(gulp.dest('wwwroot/css'))
+        .pipe(debug({ title: 'less output:' }));
 });
 
 gulp.task('default', gulp.series(['clean', 'scripts', 'less']));
