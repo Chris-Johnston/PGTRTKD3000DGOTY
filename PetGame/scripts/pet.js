fa8cfd780e205152b353cfee01c190308027f653
Chris Johnston <chjohnston@protonmail.com>
2018-12-11 01:59:43 -0800

    Fixed gulpfile and typescript, remove files that were not needed

--- PetGame/scripts/pet.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var Pet = (function () {
-    function Pet() {
-    }
-    return Pet;
-}());
-//# sourceMappingURL=pet.js.map
--- PetGame/wwwroot/scripts/login.ts
+++
@@ -1,2 +0,0 @@
-// This is an example of how to include typescript files
-// it is currently unused, but I will leave this here until we add more typescript files.
